241b8fa00961ceb0fca3f2a3dfe17c6218008bd2
amitYadavDev <amit8955461017@gmail.com>
2024-01-31 16:11:36 +0530

    Notification

---
+++ app/src/main/java/amitapps/media/service/ForegroundService.kt
@@ -0,0 +1,11 @@
+package amitapps.media.service
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class ForegroundService : Service() {
+    override fun onBind(p0: Intent?): IBinder? {
+        return null
+    }
+}
