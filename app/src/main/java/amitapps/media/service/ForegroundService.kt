8ba6ab50b59b2704d68e1d3482be13d989b4a3dd
amitYadavDev <amit8955461017@gmail.com>
2024-01-31 16:21:48 +0530

    notification with service

--- app/src/main/java/amitapps/media/service/ForegroundService.kt
+++ app/src/main/java/amitapps/media/service/ForegroundService.kt
@@ -3,9 +3,33 @@ package amitapps.media.service
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import androidx.core.app.NotificationCompat
 
 class ForegroundService : Service() {
     override fun onBind(p0: Intent?): IBinder? {
         return null
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        when(intent?.action) {
+            Actions.START.toString() -> start()
+            Actions.STOP.toString() -> stopSelf()
+        }
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    private fun start() {
+        val notification = NotificationCompat.Builder(this, "active_notification")
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("Service is running")
+            .setContentText("Hi foreground service is running, pls play with audio")
+            .build()
+
+        startForeground(1, notification)
+    }
+
+    enum class Actions {
+        START,
+        STOP
     }
 }
