8e9a64a52d99f01b9dfed88a77422c64d7018908
amitYadavDev <amit8955461017@gmail.com>
2024-01-31 16:30:51 +0530

    foreground service

---
+++ app/src/main/java/amitapps/media/service/BaseApplication.kt
@@ -0,0 +1,22 @@
+package amitapps.media.service
+
+import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+
+class BaseApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                "active_notification",
+                "Running Notification",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+}
