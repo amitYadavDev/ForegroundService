9fa8f8023b3fdcf2f1b7ead3254b8bd7f32f5624
amitYadavDev <amit8955461017@gmail.com>
2024-01-31 16:51:31 +0530

    foreground service working fine

--- app/src/main/java/amitapps/media/service/MainActivity.kt
+++ app/src/main/java/amitapps/media/service/MainActivity.kt
@@ -11,18 +11,48 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import amitapps.media.service.ui.theme.ServiceTheme
+import android.content.Intent
+import android.os.Build
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.ui.Alignment
+import androidx.core.app.ActivityCompat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+//            ActivityCompat.requestPermissions(
+//                this,
+//                arrayOf(Manifest.permission.POST_NOTIFICATION),
+//                0
+//            )
+//        }
         setContent {
             ServiceTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
+                Column (
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ){
+                    Button(onClick = {
+                        Intent(applicationContext, ForegroundService::class.java).also {
+                            it.action = ForegroundService.Actions.START.toString()
+                            startService(it)
+                        }
+                    }) {
+                            Text(text = "Start service")
+                    }
+                    Button(onClick = {
+                        Intent(applicationContext, ForegroundService::class.java).also {
+                            it.action = ForegroundService.Actions.STOP.toString()
+                            startService(it)
+                        }
+                    }) {
+                        Text(text = "Stop service")
+                    }
                 }
             }
         }
